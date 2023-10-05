e4dbf72b9bd99402eb402b5c8f12ade58c4b4731
jay bhogayata <jaybhogayata53@gmail.com>
2023-10-05 22:55:53 +0530

    refactor - handle error using some helper method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -16,7 +16,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "server is encountered a problem and cloud not process your request.", http.StatusInternalServerError)
+		app.serverErrResponse(w, r, err)
 	}
 }
