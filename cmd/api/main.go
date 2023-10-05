933ccf110858f2729f8f0988f169763b83a186d3
jay bhogayata <jaybhogayata53@gmail.com>
2023-10-05 19:27:56 +0530

    feat: health check route is working

---
+++ cmd/api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+	flag.StringVar(&cfg.env, "environment", "dev", "Environment(dev|stage|prod)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("%s server starting on port %d", cfg.env, cfg.port)
+
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jay-bhogayata/greenlight
+
+go 1.21.1
