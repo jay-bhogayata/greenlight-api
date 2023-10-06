1afdbbec2e407b62c0444de5222633ea84af3d21
jay bhogayata <jaybhogayata53@gmail.com>
2023-10-06 16:12:56 +0530

    feat : add ip based rate limit to app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,6 +26,11 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	limiter struct {
+		rps     float64
+		brust   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -44,6 +49,9 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate Limiter request per second")
+	flag.IntVar(&cfg.limiter.brust, "limiter-burst", 4, "Rate limiter maximum brust")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enable", true, "Enable rate limiter")
 
 	flag.Parse()
 
